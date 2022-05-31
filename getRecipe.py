c1eb980528da446bd10f905636604cee2906e89f
falo10 <swiecinskiolaf@gmail.com>
2022-05-31 22:15:18 +0200

    credentials instructions

--- getRecipe.py
+++ getRecipe.py
@@ -1,9 +1,13 @@
 import requests
+# credentials is a file where you should put your X-RapidAPI-Key and X-RapidAPI-Host 
+# you can take it from https://rapidapi.com/apidojo/api/tasty/
 import credentials  
 
-querystring  = {"from":"0",
+querystring  = {
+                "from":"0",
                 "size":"20",
-                "tags":"under_30_minutes"}
+                "tags":"under_30_minutes"
+                }
 
 
 response = requests.get("https://tasty.p.rapidapi.com/recipes/list" , headers = credentials.headers, params = querystring )
