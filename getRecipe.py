2dc71b3d1a1793e391a4c7840759abae33ee44f9
falo10 <swiecinskiolaf@gmail.com>
2022-06-03 10:51:45 +0200

    functions for connecting and receiving response from API

--- getRecipe.py
+++ getRecipe.py
@@ -1,20 +1,28 @@
 import requests
 # credentials is a file where you should put your X-RapidAPI-Key and X-RapidAPI-Host 
 # you can take it from https://rapidapi.com/apidojo/api/tasty/
-import credentials  
+import credentials
+import json
 
-querystring  = {
+def get_your_receipes ():
+
+    querystring  = {
                 "from":"0",
-                "size":"20",
+                "size":"1",
                 "tags":"under_30_minutes"
                 }
 
+    response = requests.get("https://tasty.p.rapidapi.com/recipes/list" , headers = credentials.headers, params = querystring )
 
-response = requests.get("https://tasty.p.rapidapi.com/recipes/list" , headers = credentials.headers, params = querystring )
+    return get_json_content_from_responde (response)
 
+def get_json_content_from_responde (response):
+    try:
+        context = response.json ()
+    except json.decoder.JSONDecodeError:
+        print ('bledny format')
+    else:
+        return context
 
-
-receipes = response.json()
-
-
-print (receipes)
+results = get_your_receipes()
+print (results)
