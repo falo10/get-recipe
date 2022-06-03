e2508f9fcf3e914070273a5b9cdb809e43c032b5
falo10 <swiecinskiolaf@gmail.com>
2022-06-03 15:10:18 +0200

    adding possibility to display instructions for making a dish

--- getRecipe.py
+++ getRecipe.py
@@ -2,7 +2,6 @@ import requests
 import credentials
 # credentials is a file where you should put your X-RapidAPI-Key and X-RapidAPI-Host 
 # you can take it from https://spoonacular.com/food-api
-
 import json
 
 
@@ -31,25 +30,64 @@ def get_json_content_from_responde (response):
 
 context = get_your_receipes()
 
+idNumber =  context['recipes'][0]['id']
 name = context['recipes'][0]['title']
 timeToPrepare = (context['recipes'][0]['readyInMinutes'])
+isVegetarian = context['recipes'][0]['vegetarian']
+isVegan = context['recipes'][0]['vegan']
+isGlutenFree = context['recipes'][0]['glutenFree']
+isDairyFree = context['recipes'][0]['dairyFree']
+instructions = context['recipes'][0]['instructions']
+url = context['recipes'][0]['sourceUrl']
+servings = context['recipes'][0]['servings']
 
-print (f""" Here is your random dish suggestion:
+listOfIngredeitns = []
 
-{name}
+instructions = instructions.replace("</li><li>", "\n\n")
+instructions = instructions.replace ("<ol><li>", "")
+instructions = instructions.replace ("</li></ol>","")
+
+print (f"""Here is your random dish suggestion:
+
+----{name}----
 
 The time to prepare this dish is:  {timeToPrepare}
 
+Additional information:
+
+Is it:
+vegetarian? : {isVegetarian}
+vegan? : {isVegan}
+gluten free? : {isGlutenFree}
+dairy free? : {isDairyFree}
+
+
 """)
 
+while True:
+    decision = input ("Would you like to check list of ingredeitns and receipe? Yes/No: ")
+    if (decision.upper() == 'YES'):
+        print ("""
+
+-------List of ingredeitns:-------    
+        """)
+        for ingredeitns in context['recipes'][0]['extendedIngredients']:
+            for key, value in ingredeitns.items():
+                if (key == 'original'):
+                    print (value)
+                    listOfIngredeitns.append(value)
+        print (f"""
+
+------- How to prepare? -------
+
+{instructions}
+        
+        
+        """)
+        break
+    elif (decision.upper() == 'NO'):
+        break
+    else:
+        print ('Wrong input')
 
 
-
-print(context['recipes'][0]['servings'])
-print(context['recipes'][0]['sourceUrl'])
-
-
-for ingredeitns in context['recipes'][0]['extendedIngredients']:
-    for key, value in ingredeitns.items():
-        if (key == 'original'):
-            print (value)
